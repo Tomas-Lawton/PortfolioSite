33efb1d0b1418ba0a984e5bb8a2bde8f96445af5
Tomas.Lawton <tomas.lawton@accenture.com>
2024-11-26 21:52:54 +1100

    fix images

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -42,7 +42,7 @@ const BlogPost = ({ post }) => {
             className="w-full h-96 rounded-lg shadow-lg object-cover"
             src={post.image}
             alt={post.title}
-          ></img>
+          ></Image>
           <h1
             ref={textOne}
             className="mt-10 text-4xl mob:text-2xl laptop:text-6xl text-bold"

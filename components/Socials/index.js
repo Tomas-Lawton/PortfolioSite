21c3bdea079d97cb1c6006b0c6f02419fec60e3b
Tomas.Lawton <tomas.lawton@accenture.com>
2024-11-30 15:20:04 +1100

    mobile centering and max  scroll point

--- components/Socials/index.js
+++ components/Socials/index.js
@@ -6,7 +6,7 @@ import yourData from "../../data/portfolio.json";
 const Socials = ({ className }) => {
   return (
     <div
-      className={`${className} flex mx-2 tablet:mx-0 flex-wrap mob:flex-nowrap link justify-center flex-col mob:flex-row`}
+      className={`${className} flex mx-2 tablet:mx-0 flex-wrap mob:flex-nowrap link justify-center tablet:justify-start flex-col mob:flex-row`}
     >
       {yourData.socials.map((social, index) => (
         <Button

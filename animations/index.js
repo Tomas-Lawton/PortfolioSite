9462d834bc670304c156928a4006c83723d769b0
Tomas.Lawton <tomas.lawton@accenture.com>
2024-12-01 17:33:47 +1100

    quicksave

--- animations/index.js
+++ animations/index.js
@@ -1,52 +1,13 @@
 // import gsap, { Power3 } from "gsap";
 import ScrambleText from 'scramble-text';
 
-// export const scramble = (element) => {
-//   console.log(element)
-//   if (element) {
-//     const resultText = element.textContent
-//     new ScrambleText( element, {
-// 		// timeOffset : 50,
-// 		callback: function () { console.log(resultText); }
-// 	} ).start();
-
-//   // scrambleInstance.start();
-
-//     // setTimeout(() => {
-//     //   scrambleInstance.stop();
-//     // }, 2000); // 2000 ms = 2 seconds
-//   }
-// };
-
-
-export const scramble = (element) => {
+export const scramble = async (element) => {
   if (element && !element.hasAttribute('data-scrambled')) {  // Check if the element is already scrambled
-    console.log(element);
-    const resultText = element.textContent;
-
-    const scrambleInstance = new ScrambleText(element, {
-      // chars: [
-      //   'W','h','e','r','e',
-      //   'C','h','a','o','s',
-      //   'M','e','e','t','s',
-      //   'C','o','d','e','',
-      //   '安','以','宇','衣','於',
-      //   '加','幾','久','計','己',
-      //   '左','之','寸','世','曽',
-      //   '太','知','川','天','止',
-      // ],
-      callback: function () {
-        console.log(resultText);  // Log the original text after scrambling
-      },
-    });
-
-    scrambleInstance.start();
-
-    // Stop the scramble effect after 2 seconds and mark the element as scrambled
+    const scrambleInstance = new ScrambleText(element).start();
     element.setAttribute('data-scrambled', 'true');  // Mark element as scrambled
     setTimeout(() => {
       scrambleInstance.stop();
-    }, 2000);  // 2000 ms = 2 seconds
+    }, 2000); 
   }
 };
 

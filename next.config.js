311159fa152c35723aa9b53b9464f612fadccd53
Tomas.Lawton <tomas.lawton@accenture.com>
2025-10-20 21:58:10 +1100

    remove cursor

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    domains: ['raw.githubusercontent.com', 'miro.medium.com', 'i.ibb.co', 'lh4.googleusercontent.com', 'drive.usercontent.google.com'], 
-  },
+    images: {
+      domains: ['raw.githubusercontent.com', 'miro.medium.com', 'i.ibb.co', 'lh4.googleusercontent.com', 'drive.usercontent.google.com'], 
+    },
 }
 
 module.exports = nextConfig
+a
